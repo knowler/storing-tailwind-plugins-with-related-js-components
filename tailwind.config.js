34edcf1b63050ecdbb68f1c88394c8b9e674d286
Nathan Knowler <nathan@knowler.me>
2019-12-04 01:45:48 -0700

    Use map instead of reduce

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const componentPlugins = glob
   .sync('./src/**/*.tailwind.js')
-  .reduce((acc, plugin) => [...acc, require(path.resolve(plugin))], []);
+  .map(plugin => require(path.resolve(plugin)));
 
 module.exports = {
   theme: {
